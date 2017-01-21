2f41413d6b5d39a5f2674151b62935eea0badd8a
Rajarshipc88 <rajarshipc@ksu.edu>
2017-01-21 03:04:04 -0600

    Delete list.jl

--- list.jl
+++
@@ -1,6 +0,0 @@
-xpoints=[1,2,-1]
-ypoints=[8,4,3,0]
-zpoints=[0,-1]
-
-points[i,j,k]=[(xpoints[i],ypoints[j],zpoints[k]) for i=1:sizeof(xpoints),j=1:sizeof(ypoints),k=1:sizeof(zpoints)]
-print(points)
